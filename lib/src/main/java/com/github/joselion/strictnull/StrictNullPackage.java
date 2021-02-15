015f360631b1382d227df3a710aa8172cf3173fb
Jose Luis Leon <joseluis5000l@gmail.com>
2021-02-15 15:49:27 -0500

    Add null check annotations (#2)

---
+++ lib/src/main/java/com/github/joselion/strictnull/StrictNullPackage.java
@@ -0,0 +1,19 @@
+package com.github.joselion.strictnull;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+@Target(ElementType.PACKAGE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Nonnull
+@TypeQualifierDefault({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+public @interface StrictNullPackage {
+
+}
