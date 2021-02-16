03bd2e5226626b48d39c46cec5833ea5a1e982d7
Jose Luis Leon <joseluis5000l@gmail.com>
2021-02-15 19:52:35 -0500

    Back to javax.annoations.Nonnull (#13)

--- src/main/java/com/github/joselion/strictnull/Nullable.java
+++ src/main/java/com/github/joselion/strictnull/Nullable.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.annotation.Nonnull;
 import javax.annotation.meta.TypeQualifierNickname;
 import javax.annotation.meta.When;
 
@@ -21,7 +22,7 @@ import javax.annotation.meta.When;
 @Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@NonNull(when = When.MAYBE)
+@Nonnull(when = When.MAYBE)
 @TypeQualifierNickname
 public @interface Nullable {
 
