813164bebfd515121361aee4fe7e8cc8dc157179
Jose Luis Leon <joseluis5000l@gmail.com>
2021-02-15 19:33:35 -0500

    NonNull implementation (#12)
    * NonNull implementation
    
    * Add missing javadoc

--- src/main/java/com/github/joselion/strictnull/NonNull.java
+++ src/main/java/com/github/joselion/strictnull/NonNull.java
@@ -1,13 +1,12 @@
 package com.github.joselion.strictnull;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-import javax.annotation.Nonnull;
-import javax.annotation.meta.TypeQualifierNickname;
+import javax.annotation.meta.TypeQualifier;
+import javax.annotation.meta.TypeQualifierValidator;
+import javax.annotation.meta.When;
 
 /**
  * Prevents for the annotated element to be {@code null}. For better results,
@@ -18,11 +17,30 @@ import javax.annotation.meta.TypeQualifierNickname;
  * @see Nullable
  * @see StrictNullPackage
  */
-@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Nonnull
-@TypeQualifierNickname
+@TypeQualifier
 public @interface NonNull {
 
+  /**
+   * Annotatation parameter to define when the annotated element cannot
+   * be assigned to {@code null}.
+   * 
+   * @return when the annotate element can be null
+   */
+  When when() default When.ALWAYS;
+
+  /**
+   * Implemetation of {@link TypeQualifierValidator} for the NonNull
+   * annotation
+   */
+  class Checker implements TypeQualifierValidator<NonNull> {
+
+    @Override
+    public When forConstantValue(final NonNull annotation, final Object value) {
+      return value == null
+        ? When.NEVER
+        : When.ALWAYS;
+    }
+  }
 }
--- src/main/java/com/github/joselion/strictnull/StrictNullPackage.java
+++ src/main/java/com/github/joselion/strictnull/StrictNullPackage.java
@@ -6,13 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.annotation.Nonnull;
 import javax.annotation.meta.TypeQualifierDefault;
 
 /**
  * A package-level annotation the adds a qualifier behavior to all fields,
  * methods, and parameters in the annotated package. The default behavior is
- * given by the {@link Nonnull} annotation. To remove this behavior on any
+ * given by the {@link NonNull} annotation. To remove this behavior on any
  * field, method, or parameter, use the {@link Nullable} annotation.
  * 
  * @since v0.0.1
@@ -22,7 +21,7 @@ import javax.annotation.meta.TypeQualifierDefault;
 @Target(ElementType.PACKAGE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Nonnull
+@NonNull
 @TypeQualifierDefault({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 public @interface StrictNullPackage {
 
